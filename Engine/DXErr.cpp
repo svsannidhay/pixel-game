2ce2b3fd3a32476c3bc235b4831b55066113860a
Sannidhay vashal <47394582+svsannidhay@users.noreply.github.com>
2019-07-07 15:28:51 +0530

    Update DXErr.cpp

--- Engine/DXErr.cpp
+++ Engine/DXErr.cpp
@@ -1,18 +1,3 @@
-//--------------------------------------------------------------------------------------
-// File: DXErr.cpp
-//
-// DirectX Error Library
-//
-// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-// PARTICULAR PURPOSE.
-//
-// Copyright (c) Microsoft Corporation. All rights reserved.
-//--------------------------------------------------------------------------------------
-
-// This version only supports UNICODE.
-
 #include "dxerr.h"
 
 #include <stdio.h>
